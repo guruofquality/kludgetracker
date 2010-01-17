c0e619f9d59d7a9c0f5b6be6043b44f8cccd4e39
jblum <jblum@5266bff0-4ee0-4f7e-8fef-64364c00c8f5>
2010-01-17 09:26:02 +0000

    Massive changes to kludge tracker:
    Made all matchers file matchers.
    Removed line matcher support and  category support.
    All matchers call a handler and pass line numbers and information tuples.
    The gr kludge tracker example app is updated to reflect these changes.
    The app also makes git calls to extract line information (like blame).
    
    
    
    git-svn-id: svn+ssh://blumhouse/svn/work/kludgetracker@102 5266bff0-4ee0-4f7e-8fef-64364c00c8f5

--- apps/gr_kludge_tracker.py
+++ apps/gr_kludge_tracker.py
@@ -3,23 +3,72 @@
 import re
 import os
 import optparse
-import commands
+import subprocess
 import kludgetracker
 
-def pyflakes_matcher(file_name):
+def command(*args):
+	return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+
+def git_command(dir, *args):
+	"""
+	Call a git command and return the output.
+	The directory must be changed before calling the command.
+	And the directory must be restored before returning.
+	"""
+	cwd = os.getcwd()
+	os.chdir(dir)
+	output = command('git', *args)
+	os.chdir(cwd)
+	return output
+
+def git_blame(file_name, lineno):
+	"""
+	Get the git blame information for a particular line in a file.
+	Return a list of (key, value) tuples with blame information.
+	"""
+	try:
+		lines = git_command(
+			os.path.dirname(file_name),
+			'blame', file_name, '--incremental',
+			'-L', '%d,%d'%(lineno+1, lineno+1),
+		).splitlines()
+		items = dict([(line+' ').split(' ', 1) for line in lines])
+		return (
+			('Committer', items['committer']),
+			('Commit ID', lines[0].split()[0]),
+		)
+	except Exception, e: return [('Error', str(e))]
+
+def pyflakes_matcher(file_name, handler):
+	"""
+	A matcher function that calls pyflakes on a file.
+	"""
 	if not file_name.endswith('.py'): return ()
-	lines = commands.getoutput('pyflakes "%s"'%file_name).splitlines()
-	results = list()
-	for line in lines:
+	for line in command('pyflakes', file_name).splitlines():
 		try:
 			lineno, message = re.match('^.*:(\d*):(.*)$', line).groups()
 			#pyflakes messages that we intend to skip:
 			if message.endswith('imported but unused'): continue
 			if message.endswith('but never used'): continue
 			if message.endswith('unable to detect undefined names'): continue
-			results.append((int(lineno)-1, message))
+			lineno = int(lineno) - 1
+			handler(lineno, ('PyFlakes', message), *git_blame(file_name, lineno))
 		except: pass
-	return results
+
+def re_line_matcher(file_name, handler):
+	"""
+	A matcher function that calls various regular expressions.
+	Each line of the file will be passed through the regexp.
+	"""
+	for lineno, line in enumerate(open(file_name, 'r').readlines()):
+		for matcher, category in (
+			(re.compile('(?i).*(#|//|/\*|\s)todo\s.*').match,   'TODO'),
+			(re.compile('(?i).*(#|//|/\*|\s)fixme\s.*').match,  'FIXME'),
+			(re.compile('(?i).*(#|//|/\*|\s)magic\s.*').match,  'Magic'),
+			(re.compile('(?i).*(#|//|/\*|\s)kludge\s.*').match, 'Kludge'),
+		):
+			if not matcher(line): continue
+			handler(lineno, ('Category', category), *git_blame(file_name, lineno))
 
 def file_matcher(file_name):
 	blacklist = ('gnuradio_swig_py_general.cc', 'Makefile.in')
@@ -31,20 +80,17 @@ if __name__ == '__main__':
 	parser = optparse.OptionParser()
 	parser.add_option("--src-dir", type="string", default=None)
 	parser.add_option("--html-dir", type="string", default='html')
-	parser.add_option("--desc", type="string", default='unknown version')
 	(options, args) = parser.parse_args()
 
 	#setup the kludge tracker parser
 	ktp = kludgetracker.parser()
-	ktp.register_line_matcher(re.compile('(?i).*(#|//|/\*|\s)todo\s.*').match,   category='TODO')
-	ktp.register_line_matcher(re.compile('(?i).*(#|//|/\*|\s)fixme\s.*').match,  category='FIXME')
-	ktp.register_line_matcher(re.compile('(?i).*(#|//|/\*|\s)magic\s.*').match,  category='Magic')
-	ktp.register_line_matcher(re.compile('(?i).*(#|//|/\*|\s)kludge\s.*').match, category='Kludge')
-	ktp.register_file_matcher(pyflakes_matcher,                                  category='PyFlakes')
+	ktp.register_matcher(pyflakes_matcher)
+	ktp.register_matcher(re_line_matcher)
 
 	#parse and generate
 	files = kludgetracker.get_matching_files(options.src_dir, file_matcher)
 	result = ktp(files, path=os.path.dirname(os.path.abspath(options.src_dir)))
-	gen = kludgetracker.generator(result, title='Gnuradio - %s'%options.desc)
+	desc = git_command(options.src_dir, 'describe')
+	gen = kludgetracker.generator(result, title='Gnuradio - %s'%desc)
 	gen.generate(options.html_dir)
 	print 'Done'
--- python/parser.py
+++ python/parser.py
@@ -2,33 +2,20 @@ import os
 import utils
 import multiprocessing
 
-class _matcher(object):
-	"""
-	Hold a line matcher function and its associated information.
-	"""
-	def __init__(self, matcher, category='default'):
-		self._matcher = matcher
-		self._category = category
-
-	def get_category(self): return self._category
-	def __call__(self, *args): return self._matcher(*args)
-
 class _chunk(object):
 	"""
 	A single line with a todofixme and its associated information.
 	"""
-	def __init__(self, file, lines, lineno, category, message):
+	def __init__(self, file, lines, lineno, info):
 		self._file = file
 		self._lines = lines
 		self._lineno = lineno
-		self._category = category
-		self._message = message
+		self._info = info
 
 	def get_file(self): return self._file
 	def get_lines(self): return self._lines
 	def get_lineno(self): return self._lineno
-	def get_category(self): return self._category
-	def get_message(self): return self._message
+	def get_info(self): return self._info
 
 class _result(object):
 	"""
@@ -36,10 +23,9 @@ class _result(object):
 	"""
 	def __init__(self, chunks): self._chunks = chunks
 
-	def get_subset(self, files=None, categories=None, subdir=None):
+	def get_subset(self, files=None, subdir=None):
 		def is_match(chunk):
 			if files is not None and chunk.get_file() in files: return True
-			if categories is not None and chunk.get_category() in categories: return True
 			if subdir is not None:
 				dirname = os.path.dirname(chunk.get_file())
 				if dirname:
@@ -51,7 +37,6 @@ class _result(object):
 	def __len__(self): return len(self.get_chunks())
 	def get_chunks(self): return self._chunks
 	def get_files(self): return set(map(_chunk.get_file, self.get_chunks()))
-	def get_categories(self): return set(map(_chunk.get_category, self.get_chunks()))
 	def get_subdirs(self):
 		subdirs = set()
 		for subdir in set(map(os.path.dirname, self.get_files())):
@@ -65,50 +50,42 @@ class parser(object):
 	A collection of matchers.
 	"""
 	def __init__(self):
-		self._line_matchers = list()
-		self._file_matchers = list()
+		self._matchers = list()
 
-	def register_line_matcher(self, *args, **kwargs):
+	def register_matcher(self, matcher):
 		"""
-		Register a new line matcher function.
-		A line matcher is a function that takes a single line
-		and returns true if the line is a match.
-		"""
-		self._line_matchers.append(_matcher(*args, **kwargs))
+		Register a new matcher function.
 
-	def register_file_matcher(self, *args, **kwargs):
+		A matcher is a function that takes two arguments:
+			1) a file path
+			2) a callable handler
+
+		The matcher should call the handler for each match,
+		passing the line number as the first argument.
+		All proceeding arguments are optional (key, value) tuples
+		that provide additional info or meta-data about the match.
+
+		The line numbering starts at index 0 for the first line.
 		"""
-		Register a new file matcher function.
-		A file matcher is a function that takes a file path
-		and returns a list of (line number, message) tuples
-		"""
-		self._file_matchers.append(_matcher(*args, **kwargs))
+		self._matchers.append(matcher)
 
 	def _get_chunks(self, files, path):
 		chunks = list()
+		#create a new handler callback
+		def handler(lineno, *info):
+			chunks.append(_chunk(
+				file = os.path.relpath(file, path),
+				lines = lines,
+				lineno = lineno,
+				info = info,
+			))
+		#call every matcher on every file
 		for file in files:
 			print 'Parsing:', os.path.abspath(file)
 			lines = open(file, 'r').readlines()
-			#call each file matcher
-			for matcher in self._file_matchers:
-				for lineno, message in matcher(file):
-					chunks.append(_chunk(
-						file = os.path.relpath(file, path),
-						lines = lines,
-						lineno = lineno,
-						category = matcher.get_category(),
-						message = message,
-					))
-			#call each line matcher
-			for matcher in self._line_matchers:
-				for i, line in enumerate(lines):
-					if matcher(line): chunks.append(_chunk(
-						file = os.path.relpath(file, path),
-						lines = lines,
-						lineno = i,
-						category = matcher.get_category(),
-						message = '',
-					))
+			#call each matcher with the handler
+			for matcher in self._matchers:
+				matcher(file, handler)
 		return chunks
 
 	def __call__(self, files, path='/'):
